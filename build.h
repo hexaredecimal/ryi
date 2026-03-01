e1d457c47d4fcc2d9ad18ac8c5ca723d62097875
hexaredecimal <mfanakagama@gmail.com>
2026-03-01 17:46:22 +0200

    build.h: Auto generated build info

---
+++ build.h
@@ -0,0 +1,6 @@
+#ifndef __BUILD_DATE__
+#define __BUILD_DATE__ "2026-03-01 17:39:48"
+#define __BUILD_BY__ "hexaredecimal"
+#define __BUILD_ON__ "Linux/Unix"
+#define __BUILD_COMMAND__ "g++ -Wall -ggdb main.cpp tinyfiledialogs.c -o ryi -lraylib"
+#endif //__BUILD_DATE__
