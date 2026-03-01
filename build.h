91b4559760db03cf7a297b9e2b85349faf37f246
hexaredecimal <mfanakagama@gmail.com>
2026-03-01 17:50:27 +0200

    build.h: Add the git repo url

--- build.h
+++ build.h
@@ -1,6 +1,7 @@
 #ifndef __BUILD_DATE__
-#define __BUILD_DATE__ "2026-03-01 17:39:48"
+#define __BUILD_DATE__ "2026-03-01 17:49:53"
 #define __BUILD_BY__ "hexaredecimal"
 #define __BUILD_ON__ "Linux/Unix"
+#define __GIT_REPO__ "https://github.com/hexaredecimal/ryi.git"
 #define __BUILD_COMMAND__ "g++ -Wall -ggdb main.cpp tinyfiledialogs.c -o ryi -lraylib"
 #endif //__BUILD_DATE__
